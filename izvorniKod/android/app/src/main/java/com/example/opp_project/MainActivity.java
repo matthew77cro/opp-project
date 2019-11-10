53a86e4f39ed7784f909c78f4403e2ebd6569f6e
realnk <klara.gudelj@fer,hr>
2019-11-10 22:18:30 +0100

    added second activity

--- izvorniKod/android/app/src/main/java/com/example/opp_project/MainActivity.java
+++ izvorniKod/android/app/src/main/java/com/example/opp_project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.opp_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,36 +16,19 @@ import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText Name;
-    private EditText Password;
-    private Button Login;
+    private EditText Name = (EditText)findViewById(R.id.textView);
+    private EditText Password = (EditText)findViewById(R.id.editText2);
+   // private Button Login = (FrameLayout)findViewById(R.id.singInBtn);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
-
-
-    Name = (EditText)findViewById(R.id.textView);
-    Password = (EditText)findViewById(R.id.editText2);
-    Login = (FrameLayout)findViewById(R.id.singInBtn);
-
-
-    Login.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view){
-            if(//poslati login na provjeru) {
-                Toast.makeText(getApplicationContext(),
-                        "Prijavljeni!",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(getApplicationContext(), "Krivo uneseni podaci",Toast.LENGTH_SHORT).show();
-
-            }
-        }
-    });
-
 
 }
 
