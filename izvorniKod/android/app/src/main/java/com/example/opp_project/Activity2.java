9611cf84f27b744d1734dcf702c9c99271b99005
realnk <klara.gudelj@fer,hr>
2019-11-10 23:38:13 +0100

    changed activitybar in toolbar

--- izvorniKod/android/app/src/main/java/com/example/opp_project/Activity2.java
+++ izvorniKod/android/app/src/main/java/com/example/opp_project/Activity2.java
@@ -1,14 +1,27 @@
 package com.example.opp_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Activity2 extends AppCompatActivity {
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 }
--- izvorniKod/android/app/src/main/java/com/example/opp_project/MainActivity.java
+++ izvorniKod/android/app/src/main/java/com/example/opp_project/MainActivity.java
@@ -16,18 +16,21 @@ import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText Name = (EditText)findViewById(R.id.textView);
-    private EditText Password = (EditText)findViewById(R.id.editText2);
+    //private EditText Name = (EditText)findViewById(R.id.textView);
+    //private EditText Password = (EditText)findViewById(R.id.editText2);
    // private Button Login = (FrameLayout)findViewById(R.id.singInBtn);
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mtoolbar = findViewById(R.id.toolbar);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("OPP banka");
+        mtoolbar.setSubtitle("Dobrodošli!");
     }
 
 }
