de0cce27c310ac8ce84e5d736e574c6757e91767
Dominik97 <dm50198@fer.hr>
2019-12-14 11:54:51 +0100

    dodano brisanje profila

--- izvorniKod/frontend/js/userProfile.js
+++ izvorniKod/frontend/js/userProfile.js
@@ -1,5 +1,6 @@
 $("#addProfileForm").hide();
 $("#updateProfileForm").hide();
+$("#deleteProfileForm").hide();
 
 $("#addProfileBtn").click(function(){
    $("#container").hide();
@@ -19,4 +20,14 @@ $("#updateProfileBtn").click(function(){
 $("#cancel2").click(function(){
    $("#container").show();
    $("#updateProfileForm").hide();
+});
+
+$("#deleteProfileBtn").click(function(){
+   $("#container").hide();
+   $("#deleteProfileForm").show();
+});
+
+$("#cancel3").click(function(){
+   $("#container").show();
+   $("#deleteProfileForm").hide();
 });
